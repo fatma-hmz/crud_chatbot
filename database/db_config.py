e907ce4081d7bf3c3043ffb023de51c76612efb1
Fatma Hamza <95277311+fatma-hmz@users.noreply.github.com>
2025-03-02 00:43:59 +0100

    Update db_config.py

--- database/db_config.py
+++ database/db_config.py
@@ -3,10 +3,21 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+#DB_CONFIG = {
+#    "dbname": os.getenv("POSTGRES_DB"),
+#    "user": os.getenv("DB_USER"),
+#    "password": os.getenv("DB_PASSWORD"),
+#    "host": os.getenv("DB_HOST"),
+#    "port": os.getenv("DB_PORT")
+#}
+
+import streamlit as st
+
+# Access database credentials from Streamlit secrets
 DB_CONFIG = {
-    "dbname": os.getenv("POSTGRES_DB"),
-    "user": os.getenv("DB_USER"),
-    "password": os.getenv("DB_PASSWORD"),
-    "host": os.getenv("DB_HOST"),
-    "port": os.getenv("DB_PORT")
+    "dbname": st.secrets["postgres"]["dbname"],
+    "user": st.secrets["postgres"]["user"],
+    "password": st.secrets["postgres"]["password"],
+    "host": st.secrets["postgres"]["host"],
+    "port": st.secrets["postgres"]["port"]
 }
